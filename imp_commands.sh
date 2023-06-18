c7d3150462be051520d733cbf305b361f8302b6a
The-Debarghya <debarghya4444@gmail.com>
2023-06-19 01:27:28 +0530

    Added etcd complex commands

---
+++ imp_commands.sh
@@ -0,0 +1,12 @@
+ETCDCTL_API=3 etcdctl --endpoints=https://[127.0.0.1]:2379 \
+--cacert=/etc/kubernetes/pki/etcd/ca.crt \
+--cert=/etc/kubernetes/pki/etcd/server.crt \
+--key=/etc/kubernetes/pki/etcd/server.key \
+snapshot save /opt/snapshot-pre-boot.db
+
+etcdctl member list --endpoints=https://127.0.0.1:2379 --cacert=/etc/etcd/pki/ca.pem --cert=/etc/etcd/pki/etcd.pem --key=/etc/etcd/pki/etcd-key.pem
+
+kubectl config get-clusters
+kubectl config use-context cluster1
+
+
